68ad4409f95b4eb3a3d1e2f1d2b137bdf3850640
Kaua4570 <150094879+Kaua4570@users.noreply.github.com>
2026-03-28 18:50:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,47 @@
-const CACHE = "app-v2";
+// NOME DO CACHE (pode mudar versão quando atualizar)
+const CACHE_NAME = "relatorio-km-v1";
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./fundo.png"
-      ]);
+// ARQUIVOS QUE SERÃO SALVOS OFFLINE
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./dados.txt",
+  "./fundo.jpg",
+  "./icon.png"
+];
+
+// INSTALAÇÃO (salva arquivos no cache)
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// ATIVAÇÃO (limpa cache antigo)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+// INTERCEPTA REQUISIÇÕES (offline)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
